370f359f4dd2d7cf65912433c1a35b707910c190
Rheik van Eyck <rheikvaneyck@yahoo.de>
2011-10-06 16:20:59 +0300

    Edited test/test_credentials.rb via GitHub

--- test/test_credentials.rb
+++ test/test_credentials.rb
@@ -4,12 +4,9 @@ require File.dirname(__FILE__) + '/../lib/Server-Health/credentials'
 
 class TestCredentials < Test::Unit::TestCase
   context "reading credentials from file" do
-    setup do
-      @c = ServerHealth::Credentials.new()
-    end
     should "return ssh user and password" do
       credential_keys = ["ssh_user", "ssh_pw", "ssh_server"]
-      credentials = @c.read_from_yaml("../config/credentials_sample.yml",credential_keys)
+      credentials = ServerHealth::Credentials.read_from_yaml("../config/credentials_sample.yml",credential_keys)
       assert_not_equal 0, credentials.length
       assert_equal "marcus", credentials[:ssh_user]
       assert_equal "geheim", credentials[:ssh_pw]
