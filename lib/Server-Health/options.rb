cb1eb1ddb7c9e2c0928ac09218e054615b8b14b9
Rheik van Eyck <rheikvaneyck@yahoo.de>
2011-10-06 15:27:26 +0300

    Edited lib/Server-Health/options.rb via GitHub

--- lib/Server-Health/options.rb
+++ lib/Server-Health/options.rb
@@ -1,83 +1,121 @@
 #!/usr/bin/env ruby
-require 'optparse'
+require_relative '../helper_class'
+require_relative 'options'
+require_relative 'credentials'
+require_relative 'log_downloader'
+require_relative 'health_file_parser'
+# require_relative 'db_connector' is obsolete!!!
+require_relative 'db_manager'
+require_relative 'chart_downloader'
+require_relative 'generate_report'
+require_relative 'generate_email'
+require_relative 'send_report'
+
 module ServerHealth
-  class Options
-    DATABASE_DIR = "db"
-    DATABASE_FILE = "health-logs.sqlite"   
-    CONFIG_DIR = "config"
-    CREDENTIAL_FILE = "credentials.yml"
-    REMOTE_LOG_DIR = "/var/log/health"
-    LOCAL_LOG_DIR = "downloads"
-    REPORT_DIR = "reports"
-    REPORT_TEMPLATE = "report_template.html.erb"
-    EMAIL_DIR = "email"
-    
-    attr_reader :database_dir
-    attr_reader :database_file
-    attr_reader :config_dir
-    attr_reader :credential_file
-    attr_reader :remote_log_dir
-    attr_reader :local_log_dir
-    attr_reader :report_dir
-    attr_reader :report_template
-    attr_reader :email_dir
+  class Runner
     
     def initialize(argv)
-      @database_dir = DATABASE_DIR
-      @database_file = DATABASE_FILE
-      @config_dir = CONFIG_DIR
-      @credential_file = CREDENTIAL_FILE
-      @remote_log_dir = REMOTE_LOG_DIR
-      @local_log_dir = LOCAL_LOG_DIR
-      @report_dir = REPORT_DIR
-      @report_template = REPORT_TEMPLATE
-      @email_dir = EMAIL_DIR
-      
-      parse(argv)
+      @options = Options.new(argv)
+      @db = ServerHealth::DBManager.new(File.join(@options.database_dir, @options.database_file))
+      @hc = Helper::HelperClass.new()
     end
-    private
-    def parse(argv)
-      OptionParser.new do |opts|
-        opts.banner = "Usage: server_health [ options ]"
-        opts.on("-d", "--db-dir path", String, "Path to directory") do |db|
-          @database_dir = db
-        end
-        opts.on("-f", "--db-file name", String, "File name") do |f|
-          @database_file = f
-        end
-        opts.on("-c", "--config-dir path", String, "Path to directory") do |c|
-          @config_dir = c
-        end
-        opts.on("-a", "--credential-file name", String, "File name") do |a|
-          @credential_file = a
-        end
-        opts.on("-r", "--remote-log-dir path", String, "Path to directory") do |r|
-          @remote_log_dir = r
-        end
-        opts.on("-l", "--local-log-dir path", String, "Path to directory") do |l|
-          @local_log_dir = l
-        end
-        opts.on("-p", "--reports-dir path", String, "Path to directory") do |p|
-          @reports_dir = p
-        end
-        opts.on("-t", "--report-template file", String, "Path to directory") do |t|
-          @report_template = t
-        end
-        opts.on("-e", "--email-dir path", String, "Path to directory") do |e|
-          @email_dir = e
-        end
-        opts.on("-h", "--help", "Show this message") do
-          puts opts
-          exit
+    
+    def run
+      # Download Log Files
+      file_list = []
+      exclude_list = @db.get_column(ServerHealth::DBManager::LogFile, :file_name) # get in here the files that are allready in the database
+      
+      cobj = ServerHealth::Credentials.new()
+    credential_keys = ["ssh_user", "ssh_pw", "ssh_server"]
+      credentials = cobj.read_from_yaml(File.join(@options.config_dir,@options.credential_file), credential_keys)
+      
+      log_downloader = ServerHealth::LogDownloader.new(credentials[:ssh_server],credentials[:ssh_user],credentials[:ssh_pw])
+      file_list = log_downloader.download_new_logs(@options.remote_log_dir, @options.local_log_dir, exclude_list)
+      
+      # Insert the log files and timestamps into the DB
+      health_data = ServerHealth::HealthData.new()
+      file_list.sort.each do |file|
+        log_file = nil
+        timestamp = ""
+        begin
+          timestamp = @hc.timestamp_from_filename(file).to_s
+          health_data.parse_health_file(File.join(@options.local_log_dir, file))
+          log_file = @db.insert_record(ServerHealth::DBManager::LogFile, {:file_name => file, :file_date => timestamp})
+          puts "Log file #{file} imported" if $DEBUG
+        rescue
+          puts "couldn't process #{file} for import in table 'log_files'"         
         end
         begin
-          # argv = ["-h"] if argv.empty?
-          opts.parse!(argv)
-        rescue OptionParser::ParseError => e
-          STDERR.puts e.message, "\n", opts
-          exit(-1)
-        end        
+          @db.insert_record(ServerHealth::DBManager::HealthState, {
+            :file_id => log_file.id,
+            :raid_state => health_data.raid_state,
+            :hd1_error_state => health_data.hd1_error_state,
+            :hd2_error_state => health_data.hd2_error_state,
+            :hd1_Raw_Read_Error_Rate => health_data.hd1_Raw_Read_Error_Rate,
+            :hd2_Raw_Read_Error_Rate => health_data.hd2_Raw_Read_Error_Rate,
+            :hd1_Reallocated_Sector_Ct => health_data.hd1_Reallocated_Sector_Ct,
+            :hd2_Reallocated_Sector_Ct => health_data.hd2_Reallocated_Sector_Ct,
+            :hd1_Offline_Uncorrectable => health_data.hd1_Offline_Uncorrectable,
+            :hd2_Offline_Uncorrectable => health_data.hd2_Offline_Uncorrectable,
+            :hd1_Reallocated_Event_Count => health_data.hd1_Reallocated_Event_Count,
+            :hd2_Reallocated_Event_Count => health_data.hd2_Reallocated_Event_Count,
+            :hd1_run_time => health_data.hd1_run_time,
+            :hd2_run_time => health_data.hd2_run_time,
+            :hd_space_used => health_data.hd_space_used,
+            :hd_space_left => health_data.hd_space_left
+          } unless log_file.nil?
+          puts "Health file #{file} imported" if $DEBUG
+        rescue
+          puts "couldn't process #{file} for import in table 'health_states"         
+        end          
       end
+            
+      # Visualize Data
+      # 1. Storage Pie
+      current_health_state = ServerHealth::DBManager::HealthState.find(:last)
+      hd_space_used = current_health_state.hd_space_used
+      hd_space_left = current_health_state.hd_space_left
+      @c = ServerHealth::StoragePie.new(hd_space_used,hd_space_left)
+      storage_pie_url = @c.get_url
+      @cd = ServerHealth::ChartDownloader.new(@options.reports_dir,"storage-")
+      storage_pie_file = @cd.download_chart(storage_pie_url)
+
+      # Generate Report
+      template_file = File.join(@options.reports_dir, @options.report_template)
+      values_reported = { 
+        :hd1_error_state => current_health_state.hd1_error_state,
+        :hd2_error_state => current_health_state.hd2_error_state,
+        :hd1_Raw_Read_Error_Rate => current_health_state.hd1_Raw_Read_Error_Rate,
+        :hd2_Raw_Read_Error_Rate => current_health_state.hd2_Raw_Read_Error_Rate,
+        :hd1_Reallocated_Sector_Ct => current_health_state.hd1_Reallocated_Sector_Ct,
+        :hd2_Reallocated_Sector_Ct => current_health_state.hd2_Reallocated_Sector_Ct,
+        :hd1_Offline_Uncorrectable => current_health_state.hd1_Offline_Uncorrectable,
+        :hd2_Offline_Uncorrectable => current_health_state.hd2_Offline_Uncorrectable,
+        :hd1_Reallocated_Event_Count => current_health_state.hd1_Reallocated_Event_Count,
+        :hd2_Reallocated_Event_Count => current_health_state.hd2_Reallocated_Event_Count,
+        :storage_pie_path => File.join(@options.reports_dir, storage_pie_file)
+      }
+      @report = ServerHealth::Report.new(template_file, values_reported)
+      report_file = Time.now.strftime("%Y-%m-%d-report.html")
+      @report.generate(File.join(@options.reports_dir,report_file))
+      
+	  # Generate E-Mail
+      images = [File.join(@options.reports_dir,storage_pie_file)]
+      @email = ServerHealth::EMail.new(images)
+	  @email.import_html(File.join(@options.reports_dir,report_file))
+	  elm_file = Time.now.strftime("%Y-%m-%d-report.elm")
+      @email.create_elm(File.join(@options.email_dir,elm_file))
+	  
+      # Send Report
+      credential_keys = ["smtp_user", "smtp_pw", "smtp_server", "smtp_port", "smtp_host", "email_from", "email_to"]
+      @credentials = c.read_from_yaml(File.join(@options.config_dir,@options.credential_file),credential_keys)
+      @email_sender = ServerHealth::MailSender.new(File.join(@options.email_dir,elm_file), @credentials)
+	  subject = "Server Health Report vom #{Time.now.strftime("%d. %m. %Y")}"
+      email_envelop = "From: #{credentials[:email_from]}\nTo: #{credentials[:email_to]}\nSubject: #{subject}\n"
+      @email_sender.mail_text = email_envelop + @email_sender.mail_text
+      @email_sender.send_report(credentials[:email_from], [credentials[:email_to]])
+	  
+      puts "Finished."
     end
   end
 end
