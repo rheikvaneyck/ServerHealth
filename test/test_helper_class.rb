c1f473105cd723facbe151d2bab2d214adf66ccf
Rheik van Eyck <rheikvaneyck@yahoo.de>
2011-10-06 16:19:57 +0300

    Edited test/test_helper_class.rb via GitHub

--- test/test_helper_class.rb
+++ test/test_helper_class.rb
@@ -3,11 +3,8 @@ require File.dirname(__FILE__) + '/../lib/helper_class'
 
 class TestHelperClass < Test::Unit::TestCase
   context "helper class methods" do
-    setup do
-      @hc = Helper::HelperClass.new()
-    end
     should "return success of parsing file" do
-      assert_equal "2011-03-18 06:00:00 +0100", @hc.timestamp_from_filename("2011-03-18-HealthStatus.log").to_s
+      assert_equal "2011-03-18 06:00:00 +0100", Helper::HelperClass.timestamp_from_filename("2011-03-18-HealthStatus.log").to_s
     end
   end
 end
